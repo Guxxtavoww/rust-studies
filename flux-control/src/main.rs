40173a2af29550179c7a85fda445ec0c755b2177
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-05-03 11:33:35 -0300

    Adding stuff

--- flux-control/src/main.rs
+++ flux-control/src/main.rs
@@ -1,7 +1,12 @@
+mod loop_file;
 mod ternary;
+mod while_file;
 
 fn main() {
     let test = ternary::ternary_fn(10 > 0);
 
     println!("Resultado ternario: {}", test);
+
+    // loop_file::loop_fn();
+    while_file::while_fn(100);
 }
