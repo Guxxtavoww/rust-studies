93a90080126fdd0284222d3e6ddb50db56d6a91b
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-05-09 14:48:14 -0300

    Adding stuff

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,11 @@
-mod panic_function;
+// mod panic_function;
+mod read_file;
 
 fn main() {
-    panic_function::panic_catch();
+    // panic_function::panic_catch();
+
+    match read_file::read_file("text.txt") {
+        Ok(content) => println!("File content: {}", content),
+        Err(err) => eprintln!("Error reading file: {}", err),
+    }
 }
