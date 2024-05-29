be0989d97da0e1a037f51726f5e9a171acf8e5ee
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-05-29 17:07:00 -0300

    Adding stuff

---
+++ lib-crates/README.md
@@ -0,0 +1,5 @@
+Esse é um projeto feito para meu entendimento de bibliotecas com rust e crates.io
+
+# Comando para iniciar uma nova lib
+
+`cargo init --lib`
---
+++ lib-crates/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn subtract(left: usize, right: usize) -> usize {
+    left - right
+}
