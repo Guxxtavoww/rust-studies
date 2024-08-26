5133aaf5e91b7ee00d59b149cd72116ab59e862b
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-08-26 13:56:22 -0300

    first commit

--- user-interaction/src/main.rs
+++ user-interaction/src/main.rs
@@ -3,11 +3,11 @@ use std::io::stdin;
 fn main() {
     let mut user_input = String::new();
 
-    stdin().read_line(&mut user_input).ok().expect("Erro ao ler a linha");
-
-    if user_input.is_empty() {
-        
+    match stdin().read_line(&mut user_input) {
+        Ok(n) => {
+            println!("{n} bytes read");
+            println!("Texto inserido: {user_input}");
+        }
+        Err(error) => println!("error: {error}"),
     }
-
-    println!("Texto inserido: {}", user_input)
 }
