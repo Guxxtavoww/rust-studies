2102a8f14a0715376462e40b4927de51a46f3cef
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-08-28 11:34:08 -0300

    Chore: adding stuff

--- reverse-array/src/main.rs
+++ reverse-array/src/main.rs
@@ -1,11 +1,15 @@
 mod array_utils;
 
-use array_utils::reverse_array;
+use array_utils::{ reverse_array, sort_array };
 
 fn main() {
-    let mut arr = [1, 2, 3, 4, 5];
+    let mut arr = [1, 2, 3, 4, 5, 3];
 
     reverse_array(&mut arr);
 
     println!("{:?}", arr);
+
+    sort_array(&mut arr, "desc");
+
+    println!("Sorted: {:?}", arr)
 }
